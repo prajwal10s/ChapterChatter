86944d24596a161a59e77a3ac9d0330ed5ff4e6f
Arjun Dixit <arjun.dixit98@gmail.com>
2024-04-21 03:52:41 +0530

    Added user model

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
+const { connectToMongoDB } = require("./connection");
+const { userModel } = require("./models/user");
+
+connectToMongoDB(process.env.MONGODB_URL).then(() => {
+  console.log("Mongo DB connected");
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port : ${PORT}`);
