28c750a125644f04f033d980329d4c8709b35662
Arjun Dixit <arjun.dixit98@gmail.com>
2024-04-21 20:01:13 +0530

    connected user with mongodb' '

--- models/user.js
+++ models/user.js
@@ -7,11 +7,6 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
     fullname: {
       type: String,
       required: true,
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,56 @@
 const express = require("express");
 const router = express.Router();
+const userModel = require("../models/user");
 
-router.get("/", (req, res) => {
+//controllers
+const handleCreateUser = async (req, res) => {
+  const { fullname, username, password } = req.body;
+  //find whether user already exists
+  const findUser = await userModel.findOne({ username });
+  if (findUser) {
+    console.log(`User ${username} already exists`);
+  } else {
+    await userModel.create({
+      username,
+      fullname,
+      password,
+    });
+  }
+  return res.redirect("login");
+};
+
+const handleLoginUser = async (req, res) => {
+  const { username, password } = req.body;
+  //console.log(username);
+  if (!username || !password) {
+    return res.redirect("/login");
+  }
+
+  const findUser = await userModel.findOne({ username, password });
+  if (!findUser) {
+    return res
+      .status(400)
+      .json({ status: "error", msg: "Either username or pwd is incorrect!" });
+  }
+  return res.render("comment", { username });
+};
+
+//routes..........
+
+//render signup page
+router.get("/signup", (req, res) => {
   return res.render("signup");
 });
+
+//create user into db and redirect to login page
+router.post("/createUser", handleCreateUser);
+
+//render login page
+router.get("/login", (req, res) => {
+  return res.render("login");
+});
+
+//authenticate and render comment page
+router.post("/login", handleLoginUser);
 
 module.exports = router;
