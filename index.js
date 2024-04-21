2d2a27e65b3efe6e341cc7dc77e6bde6e374c1d4
Yadavendra Sakharkar <yadavendra@Yadavendras-MacBook-Air.local>
2024-04-21 20:05:31 +0530

    connecte books and comments with mongodb

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const PORT = process.env.PORT;
 const path = require("path");
 const app = express();
 const commentRoute = require("./routes/comment.js");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const connectToMongoDB = require("./connection");
 const userModel = require("./models/user");
 const bookModel = require("./models/book");
+const commentModel = require("./models/comment");
 const userRoute = require("./routes/user");
 
 app.set("views", path.join(__dirname + "/views"));
