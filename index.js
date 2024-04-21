52469ea872911ba442a4c5bea57433879159c872
Arjun Dixit <arjun.dixit98@gmail.com>
2024-04-21 20:01:13 +0530

    connected user with mongodb' '

--- index.js
+++ index.js
@@ -4,14 +4,35 @@ const express = require("express");
 const PORT = process.env.PORT;
 const path = require("path");
 const app = express();
+const session = require("express-session");
 const commentRoute = require("./routes/comment.js");
 const connectToMongoDB = require("./connection");
 const userModel = require("./models/user");
 const bookModel = require("./models/book");
 const userRoute = require("./routes/user");
+const { execArgv } = require("process");
 
 app.set("views", path.join(__dirname + "/views"));
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+// app.use(
+//   session({
+//     secret: "ChapterChatter",
+//     resave: false,
+//     saveUninitialized: true,
+//   })
+// );
+
+// const isLoggedIn = (req, res, next) => {
+//   if (req.session.user) {
+//     console.log(`User ${user.username} already logged in`);
+//   } else {
+//     console.log(`User not logged in`);
+//   }
+//   next();
+// };
+
+// app.use(isLoggedIn);
 
 connectToMongoDB(process.env.MONGODB_URL).then(() => {
   console.log("Mongo DB connected");
